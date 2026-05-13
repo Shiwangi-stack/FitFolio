dbe7ac30db4cdedb516dd9660ae889134eab9de2
Shiwangi-stack <shiwangishiwa2000@gmail.com>
2026-05-13 21:57:22 +0530

    fixed backend urls

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const mongoose=require('mongoose')
 require('dotenv').config()
 
 const app=express()
+app.use("/images", express.static("public/images"));
 app.use(cors())
 app.use(express.json())
 
