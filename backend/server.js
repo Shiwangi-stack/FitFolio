04b7f7b9027ee386362a2793c8593d06c984ddc5
Shiwangi-stack <shiwangishiwa2000@gmail.com>
2026-05-14 11:29:10 +0530

    fixed static image serving

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,13 @@ const express=require('express')
 const cors=require('cors')
 const mongoose=require('mongoose')
 
-
+const path = require("path");
 
 const app=express()
-app.use("/images", express.static("public/images"));
+app.use(
+  "/images",
+  express.static(path.join(__dirname, "public/images"))
+);
 app.use(cors())
 app.use(express.json())
 
